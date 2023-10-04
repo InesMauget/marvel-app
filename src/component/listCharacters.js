7ca394c9fbf7067fa244b71813b870b60b9ff741
InesMauget <ines.mauget@etu.univ-poitiers.fr>
2023-10-04 18:18:25 +0200

    🚧 ajout fonctionalité clic perso + info

--- src/component/listCharacters.js
+++ src/component/listCharacters.js
@@ -1,18 +1,47 @@
-const characters=require('../data/characters.json');
-function CharactersList(){
-    return(
-      <>
-      <h3>List of characters :</h3>
-      <ul>
-      {characters.map(character => (
-              <li key={character.id}>
-                <p>{character.name}</p>
-              </li>
-            ))}
-      </ul>
-      </>
-    )
+import React, { useState } from 'react';
+
+const characters = require('../data/characters.json');
+
+function CharactersList() {
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+
+  const handleClick = (character) => {
+    setSelectedCharacter(character);
   };
 
+  return (
+    <>
+      <h3>List of characters :</h3>
+      <ul>
+        {characters.map((character) => (
+          <li
+            key={character.id}
+            onClick={() => handleClick(character)}
+          >
+            <p>{character.name}</p>
+          </li>
+        ))}
+      </ul>
+      {selectedCharacter && (
+        <div>
+          <h4>Nom :</h4>
+          {selectedCharacter.name}
+          <br></br>
+          <h4>Image : </h4>
+          {selectedCharacter.thumbnail && (
+            <img
+              src={`${selectedCharacter.thumbnail.path}.${selectedCharacter.thumbnail.extension}`}
+              alt={selectedCharacter.name}
+              style={{ maxWidth: '100px' }}
+            />
+          )}
+          <br />
+          <h4>Description :</h4>
+          {selectedCharacter.description}
+        </div>
+      )}
+    </>
+  );
+}
 
-  export default CharactersList;
+export default CharactersList;
